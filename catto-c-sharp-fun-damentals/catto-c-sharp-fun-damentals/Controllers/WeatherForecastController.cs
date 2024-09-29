e2b476a369e4a8867fc0f7e514ddf9a1261d544d
Catto <catto@live.com>
2024-09-29 14:03:16 -0400

    some fun alters.

--- catto-c-sharp-fun-damentals/catto-c-sharp-fun-damentals/Controllers/WeatherForecastController.cs
+++ catto-c-sharp-fun-damentals/catto-c-sharp-fun-damentals/Controllers/WeatherForecastController.cs
@@ -31,3 +31,32 @@ public class WeatherForecastController : ControllerBase
     }
 }
 
+[ApiController]
+[Route("[controller]")]
+public class FunController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<FunController> _logger;
+
+    public FunController(ILogger<FunController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetFun")]
+    public IEnumerable<Fun> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new Fun
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
+
